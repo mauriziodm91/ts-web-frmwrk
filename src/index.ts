f9004b240b8359ecc90e12b61aecd34fc3e9674d
Maurizio <mauriziodm91@gmail.com>
2023-03-29 14:10:12 -0300

    used composition pattern

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { User } from './models/Users'
 
-const user = new User({ name: 'Charles', age: 50 })
+const user = new User({ id: 1, name: 'newName', age: 0 })
+
+user.on('save', () => {
+  console.log(user)
+})
+
+user.save()
